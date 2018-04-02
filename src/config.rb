b879754c9b62f24f426baca9ec09e58f6a9255bc
ognan <bazieambroise@gmail.com>
2018-04-02 21:50:04 +0100

    first commit

---
+++ src/config.rb
@@ -0,0 +1,5 @@
+http_path = '/'
+css_dir = 'static/css/'
+sass_dir = 'static/scss/'
+images_dir = 'static/img/'
+javascript_dir = 'static/js/'
---
+++ src/management/urls.py
@@ -0,0 +1,31 @@
+"""management URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import re_path
+from django.conf import  settings
+from django.conf.urls.static import static
+from .views import HomepageView
+
+urlpatterns = [
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^$', HomepageView.as_view(), name='login')
+]
+
+# Include Media and static files when DEBUG=True
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
